5fb7e12f8fbefc72be6368f6dbc4458bb01a7cdb
Cole Sullivan <coles@mailbox.org>
2024-02-15 15:15:10 -0600

    Updated docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /QUAD/tmp/pids/server.pid
+
+# Add this line before the exec command
+export PATH="$PATH:/QUAD/bin"
+
 # If running the rails server then create or migrate existing database
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-  ./bin/rails db:prepare
+  echo "Preparing database..."
+  bundle exec rails db:prepare
+  echo "Database prepared."
 fi
 
 exec "${@}"
