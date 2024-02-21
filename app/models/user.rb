703eeea53a0bc837998296ca4e2ad5cca5d89c6e
mdreis <126283862+mdreis@users.noreply.github.com>
2024-02-21 15:30:36 -0600

    Create user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+    
+
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
+  has_secure_password
+
+  before_save :downcase_fields
+
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@crimson\.ua\.edu\z/, message: "must be a crimson.ua.edu account" }, presence: true, uniqueness: true
+  validates :username, :phone_number, presence: true, uniqueness: true
+  validates:first_name, :last_name, presence: true
+
+  def confirm!
+    update_columns(confirmed_at: Time.current)
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def generate_confirmation_token
+    signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+  end
+
+  def unconfirmed?
+    !confirmed?
+  end
+
+  private
+
+    def downcase_fields
+      self.email = email.downcase
+      self.first_name = first_name.downcase
+      self.last_name = last_name.downcase
+      self.username = username.downcase
+    end
+  end
