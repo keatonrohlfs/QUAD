c7571da5a5acf356d3d9ebf4c2c5b6b017246c2d
mdreis <mdreis@crimson.ua.edu>
2024-03-20 20:09:37 -0500

    small fix

--- app/controllers/active_sessions_controller.rb
+++ app/controllers/active_sessions_controller.rb
@@ -16,6 +16,7 @@ class ActiveSessionsController < ApplicationController
 
   def destroy_all
     current_user.active_sessions.destroy_all
+    forget_active_session
     reset_session
 
     redirect_to root_path, notice: "Signed out."
