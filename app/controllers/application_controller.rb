3bda30671d7f702fb872565cdb91c2c99a1aa08b
mdreis <mdreis@crimson.ua.edu>
2024-04-24 18:20:14 -0500

    final  part of shopify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,37 @@
 class ApplicationController < ActionController::Base
-    include Authentication
+  include Authentication
+  ShopifyAPI::Context.setup(
+    api_key: ENV['SHOPIFY_API_KEY'],
+    api_secret_key: ENV['SHOPIFY_API_SECRET'],
+    api_version: ENV['SHOPIFY_API_VERSION'], # The version of the API you would like to use
+    scope: [
+      'read_customers',
+      'write_customers',
+      'read_fulfillments',
+      'write_fulfillments',
+      'read_inventory',
+      'write_inventory',
+      'write_order_edits',
+      'read_order_edits',
+      'write_orders',
+      'read_orders',
+      'write_products',
+      'read_products',
+    ],
+    is_embedded: false, # Set to true if you are building an embedded app
+    is_private: true # Set to true if you are building a private app
+    )
+  def clear_flash
+    flash.discard # Or flash.clear to remove all flash values
+    redirect_back(fallback_location: root_path)
+  end
 
-    def clear_flash
-      flash.discard # Or flash.clear to remove all flash values
-      redirect_back(fallback_location: root_path)
-    end
-
-    private
-      def require_login
-        unless current_user
-          redirect_to login_path
-        end
+  private
+    def require_login
+      unless current_user
+        redirect_to login_path
       end
-end
+    end
+  end
+
+
